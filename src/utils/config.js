817fb90969aa997f449ddab1ae531110a863fdbf
Nive77 <118598936+Nive77@users.noreply.github.com>
2023-10-29 14:50:36 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://travel-back-zswi.onrender.com'
+export const BASE_URL = 'https://travel-se-be-h6cm.vercel.app/'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
